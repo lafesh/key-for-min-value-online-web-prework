06cd08519d4801adf7450c49e8514f7f710a20f9
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 04:01:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,12 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   else 
-    name_hash.collect do |key, value|
-    value <=> value
-    return 
+    smallest_value = nil
+    smallest_key = nil
+    name_hash.each do |key, value|
+    if value < smallest_value
+      smallest_value = value
+      smallest_key = key
     end
+    smallest_key
 end
